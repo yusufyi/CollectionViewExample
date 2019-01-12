d678fc6de4e61d68ef7c95e9a4b2ab1740232ee9
yusuf <yusufyi@hotmail.com>
2019-01-12 21:47:05 +0300

    collectionView example

---
+++ CollectionViewTest/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  CollectionViewTest
+//
+//  Created by Yusuf Yigit on 12.01.2019.
+//  Copyright © 2019 Yusuf Yigit. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
--- CollectionViewTest/ViewController.swift
+++ CollectionViewTest/ViewController.swift
@@ -8,13 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UICollectionViewDelegate , UICollectionViewDataSource{
+   
+    
+    
 
+    
+    
+    let array:[String] = ["alien-1","alien-2","alien-3","alien-4","alien-5"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CollectionViewCell
+        
+        cell.imageView.image = UIImage(named: array[indexPath.row] + ".png")
+        return cell
+        
+    }
 
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print (array[indexPath.row])
+    }
 
 }
 
